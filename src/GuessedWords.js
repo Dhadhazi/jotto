3a2371e242017072bafec733ff4e90071b1e388d
Dhadhazi <dhadhazi@gmail.com>
2020-10-11 13:33:04 +0200

    Start tests for Guesswords

---
+++ src/GuessedWords.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const GuessedWords = (props) => {
+  return (
+    <div data-test="component-guessed-words">
+      {props.guessedWords.length === 0 ? (
+        <span data-test="guess-instructions">Try to guess the secret word</span>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
