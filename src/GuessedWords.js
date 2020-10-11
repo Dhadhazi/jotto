1d38168a8bb7b8824330deb5da3a3231b17c72f2
Dhadhazi <dhadhazi@gmail.com>
2020-10-11 14:06:35 +0200

    added bootstrap

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -7,7 +7,25 @@ export const GuessedWords = (props) => {
       {props.guessedWords.length === 0 ? (
         <span data-test="guess-instructions">Try to guess the secret word</span>
       ) : (
-        ""
+        <div data-test="guessed-words">
+          <h3>Guessed Words</h3>
+          <table className="table table-sm">
+            <thead className="thead-light">
+              <tr>
+                <th>Guess</th>
+                <th>Matching Letters</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.guessedWords.map((word, index) => (
+                <tr key={index} data-test="guessed-word">
+                  <td>{word.guessedWord}</td>
+                  <td>{word.letterMatchCount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )}
     </div>
   );
