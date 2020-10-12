6e82786a5f447c411040aa2c4de023ce8b21de25
Dhadhazi <dhadhazi@gmail.com>
2020-10-12 13:44:47 +0200

    Input field added with test

--- src/Input.js
+++ src/Input.js
@@ -2,7 +2,25 @@ import React from "react";
 import PropTypes, { string } from "prop-types";
 
 export const Input = ({ secretWord }) => {
-  return <div data-test="component-input"></div>;
+  const [currentGuess, setCurrentGuess] = React.useState("");
+
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-w mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button data-test="submit-button" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 Input.propTypes = {
