b9041baaa69153f59e444cfb9c656117340c0b7f
Dhadhazi <dhadhazi@gmail.com>
2020-10-12 16:39:52 +0200

    added reducer to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { GuessedWords } from "./GuessedWords";
-import { Congrats } from "./Congrats";
+import hookActions from "./actions/hookActions";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload };
+    default:
+      throw new Error("invalid action type");
+  }
+}
 
 function App() {
-  return (
-    <div className="container" data-test="component-app">
-      <h1>Jotto</h1>
-      <Congrats success={true} />
-      <GuessedWords
-        guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-      />
-    </div>
-  );
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+
+  const setSecretWord = (secretWord) =>
+    dispatch({ type: "setSecretWord", payload: secretWord });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
+
+  return <div className="container" data-test="component-app"></div>;
 }
 
 export default App;
